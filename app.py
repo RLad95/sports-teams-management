d4abf017dcf19fa1bcccaef2708baca53f777ec4
vagrant <raj95_lad@hotmail.com>
2017-08-23 06:13:16 +0000

    db blocked moved outside of mail

--- app.py
+++ app.py
@@ -17,13 +17,14 @@ app.register_blueprint(player_url)
 app.register_blueprint(user_url)
 app.register_blueprint(json_url)
 
+db.init_app(app)
+
 @app.before_first_request
 def create_tables():
     db.create_all()
 
 if __name__ == '__main__':
-
-    db.init_app(app)
+    
     app.secret_key = 'super_secret_key'
     app.debug = True
     app.run(host='0.0.0.0', port=5000)
